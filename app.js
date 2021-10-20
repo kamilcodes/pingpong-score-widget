ce4abc84f1b6836ea80a5bb8d07badfe4addc3ab
kamilcodes <kc.chrzanowski@gmail.com>
2021-10-20 11:48:42 +0200

    Deleted render() & moved to individual player counter click events. Refactored checkScore() and added new conditionals

--- app.js
+++ app.js
@@ -8,52 +8,38 @@ const btnReset = document.querySelector("#btnReset");
 // initial states
 let scoreCounterP1 = 0;
 let scoreCounterP2 = 0;
-renderPoints();
+let maxScore = 11;
 
-function renderPoints() {
-  scorePlayer1.textContent = scoreCounterP1;
-  scorePlayer2.textContent = scoreCounterP2;
-  checkScore();
-  return;
-}
+// set max score
+maxScoreSelect.addEventListener("change", (e) => {
+  maxScore = maxScoreSelect.value;
+});
 
-// add points
+// count & render points
 btnP1.addEventListener("click", () => {
   scoreCounterP1++;
-  renderPoints();
+  scorePlayer1.textContent = scoreCounterP1;
+  checkScore();
 });
 btnP2.addEventListener("click", () => {
   scoreCounterP2++;
-  renderPoints();
-});
-
-// reset score keeper
-btnReset.addEventListener("click", () => {
-  let resetPoints = confirm("Do you really want to reset the score?");
-  if (resetPoints) {
-    scoreCounterP1 = 0;
-    scoreCounterP2 = 0;
-    renderPoints();
-    btnP1.disabled = false;
-    btnP2.disabled = false;
-    scorePlayer1.style.color = "black";
-    scorePlayer2.style.color = "black";
-  }
+  scorePlayer2.textContent = scoreCounterP2;
+  checkScore();
 });
 
 //check who wins
 function checkScore() {
   if (
-    scoreCounterP1 === Number(maxScoreSelect.value) &&
-    scoreCounterP2 !== Number(maxScoreSelect.value)
+    scoreCounterP1 >= maxScore &&
+    Math.abs(scoreCounterP1 - scoreCounterP2) >= 2
   ) {
     scorePlayer1.style.color = "green";
     scorePlayer2.style.color = "red";
     btnP1.disabled = true;
     btnP2.disabled = true;
   } else if (
-    scoreCounterP1 !== Number(maxScoreSelect.value) &&
-    scoreCounterP2 === Number(maxScoreSelect.value)
+    scoreCounterP2 >= maxScore &&
+    Math.abs(scoreCounterP2 - scoreCounterP1) >= 2
   ) {
     scorePlayer2.style.color = "green";
     scorePlayer1.style.color = "red";
@@ -61,3 +47,18 @@ function checkScore() {
     btnP2.disabled = true;
   }
 }
+
+// reset score keeper
+btnReset.addEventListener("click", () => {
+  let resetPoints = confirm("Do you really want to reset the score?");
+  if (resetPoints) {
+    scoreCounterP1 = 0;
+    scoreCounterP2 = 0;
+    scorePlayer1.textContent = scoreCounterP1;
+    scorePlayer2.textContent = scoreCounterP2;
+    btnP1.disabled = false;
+    btnP2.disabled = false;
+    scorePlayer1.style.color = "black";
+    scorePlayer2.style.color = "black";
+  }
+});
